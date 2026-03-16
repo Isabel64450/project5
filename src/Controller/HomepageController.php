7ba773c31fff69de55a82f261a6b71ae89af7d8a
isabel64450 <isa.dumas64@gmail.com>
2026-03-16 14:49:29 +0100

    paginator works

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,19 +5,30 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use App\Repository\SubCategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
-    {
-       return $this->render('homepage/index.html.twig', [
-           'products' => $productRepository->findAll()
-    
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginator ): Response
+    {  
 
+         $data = $productRepository->findby([],['id'=>"DESC"]);
+        $products = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),//met en place la pagination
+            2 //je choisi la limite de 8 articles par page
+        );
+
+       return $this->render('homepage/index.html.twig', [
+           'products' => $products,
+            
+            'categories'=>$categoryRepository->findAll()
         ]);
     }
 
@@ -32,7 +43,29 @@ final class HomepageController extends AbstractController
         return $this->render('homepage/show.html.twig', [ 
             'product'=>$product,
             'products'=>$lastProductsAdd,
-            'categories'=>$categoryRepository->findAll()
+            'categories'=>$categoryRepository->findAll(),
         ]);
     } 
+
+      #[Route('/product/subcategory/{id}/filter ', name: 'app_home_product_filter', methods: ['GET'])]
+        public function filter($id, SubCategoryRepository $subCategoryRepository, CategoryRepository $categoryRepository): Response 
+    
+    {  
+        $product = $subCategoryRepository->find($id)->getProducts(); 
+      
+        $subCategory = $subCategoryRepository->find($id);
+        
+        return $this->render('homepage/filter.html.twig', [ 
+        'products'=>$product, 
+        'subCategory'=>$subCategory,
+        'categories'=>$categoryRepository->findAll()
+        ]);
+    }
+
+
+
+
+
+
+
 }
