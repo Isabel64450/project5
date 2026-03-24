65ed55c3a7e7f745c934f8d11aa11f349dc39e4f
isabel64450 <isa.dumas64@gmail.com>
2026-03-24 16:50:23 +0100

    email runs and bill pdf print

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -15,10 +15,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class OrderController extends AbstractController
-{
+{    
+     public function __construct(private MailerInterface $mailer){
+    }
+
+
     #[Route('/order', name: 'app_order')]
     public function index(Request $request, SessionInterface $session, ProductRepository $productRepository, EntityManagerInterface $entityManager, Cart $cart): Response
     {   
@@ -47,6 +53,14 @@ final class OrderController extends AbstractController
          }
 
           $session->set('cart',[]);
+          $html = $this->renderView('mail/orderConfirm.html.twig',['order'=>$order]);
+          $email = (new Email())
+          ->from('izaberu.creations@gmail.com')
+          ->to('test@gmail.com')
+          ->subject('Confirmation of order receipt')
+          ->html($html);
+          $this->mailer->send($email);
+         
           return $this->redirectToRoute('app_order_message');  
 
         }              
